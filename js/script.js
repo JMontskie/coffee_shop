bf9d87f1935cc75d24fa609b52099857eba9c430
Joseph Monton <35918470+JMontskie@users.noreply.github.com>
2022-10-08 02:48:04 +0800

    created new html for redesign

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function taxCalc(amount){
+    var cash;
+    var tax  = 0.825;
+    let result = amount * 0.825;
+    return result; 
+    console.log(result);
+}
+
+// console.log(taxCalc(500));
+taxCalc(500);
+
+// var taxcalc = {
+//     amount 
+// }
+
+
+
+
+var functionOne = function() {
+  console.log("Hello!");
+};
+
+functionOne();
